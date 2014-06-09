eec3b5596229c844a228c4fafc11d8a6aa95c97a
Mayukh Das <maydas@indiana.edu>
2014-06-09 16:45:39 -0400

    Changing User Perf Db

--- openwords_android_client/src/com/openwords/DAO/UserPerfDbHelper.java
+++ openwords_android_client/src/com/openwords/DAO/UserPerfDbHelper.java
@@ -1,6 +1,13 @@
 package com.openwords.DAO;
 
+import java.util.ArrayList;
+
+import com.openwords.dto.PlateDbDto;
+import com.openwords.dto.UserPerfDto;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -46,6 +53,50 @@ public class UserPerfDbHelper extends SQLiteOpenHelper{
 	}
 	
 	//-----------------CRUDs for User Perf----------
+	public void addUserPerf(UserPerfDto up)
+	{
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues values = new ContentValues();
+        values.put(OpenwordsDatabaseManager.UserPerfDB.CONNECTIONID,Integer.toString(up.connection_id));
+        values.put(OpenwordsDatabaseManager.UserPerfDB.USERID, Integer.toString(up.user_id));
+        values.put(OpenwordsDatabaseManager.UserPerfDB.TOTALCORRECT, Integer.toString(up.total_correct));
+        values.put(OpenwordsDatabaseManager.UserPerfDB.TOTALSKIPPED, Integer.toString(up.total_skipped));
+        values.put(OpenwordsDatabaseManager.UserPerfDB.TOTALEXPOSURE, Integer.toString(up.total_skipped));
+        values.put(OpenwordsDatabaseManager.UserPerfDB.LASTTIME, Integer.toString(up.last_time));
+        values.put(OpenwordsDatabaseManager.UserPerfDB.LASTPERFORMANCE, Integer.toString(up.last_performance));
+        values.put(OpenwordsDatabaseManager.UserPerfDB.USEREXCLUDE, "");
+        
+        db.insert(OpenwordsDatabaseManager.UserPerfDB.TABLE_NAME, null, values);
+		db.close();
+	}
 	
+	public ArrayList<UserPerfDto> getAll()
+	{
+		SQLiteDatabase db = this.getReadableDatabase();
+		ArrayList<UserPerfDto> result = new ArrayList<UserPerfDto>();
+		
+		Cursor cursor = db.query(OpenwordsDatabaseManager.UserPerfDB.TABLE_NAME, 
+				new String[] {OpenwordsDatabaseManager.UserPerfDB.CONNECTIONID,
+				OpenwordsDatabaseManager.UserPerfDB.USERID,
+				OpenwordsDatabaseManager.UserPerfDB.TOTALCORRECT,
+				OpenwordsDatabaseManager.UserPerfDB.TOTALSKIPPED,
+				OpenwordsDatabaseManager.UserPerfDB.TOTALEXPOSURE,
+				OpenwordsDatabaseManager.UserPerfDB.LASTTIME,
+				OpenwordsDatabaseManager.UserPerfDB.LASTPERFORMANCE,
+				OpenwordsDatabaseManager.UserPerfDB.USEREXCLUDE}, null, null, null, null, null);
+		 if (cursor != null)
+	            cursor.moveToFirst();
+		 while(!cursor.isAfterLast())
+	        {
+			 UserPerfDto newPerf = new UserPerfDto(cursor.getInt(0),cursor.getInt(1),cursor.getInt(2),
+	        			cursor.getInt(3),cursor.getInt(4), cursor.getInt(5),cursor.getInt(6),cursor.getInt(7));
+			 	result.add(newPerf);
+	        	cursor.moveToNext();
+	        }
+	        db.close();	
+	        
+	        return result;
+		
+	}
 
 }
