7d10572d5edf0999d48ba9a2e31672d144bb1a0e
Mayukh Das <maydas@indiana.edu>
2014-06-30 14:50:32 -0400

    Getting Word Transcriptions

--- openwords_android_client/src/com/openwords/model/WordTranscription.java
+++ openwords_android_client/src/com/openwords/model/WordTranscription.java
@@ -22,10 +22,20 @@ public class WordTranscription extends SugarRecord<WordTranscription>{
 		this.transcription=transcription;
 	}
 	
-	public List<WordTranscription> findByWord(int wordL2Id)
+	public static List<WordTranscription> findByWord(int wordL2Id)
 	{
 		List<WordTranscription> trans = WordTranscription.find(WordTranscription.class, "word_l_two_id="+wordL2Id);
 		return trans;
+	}
+	
+	public static void insertMerge(Context ctx, int word, String trans)
+	{
+		List<WordTranscription> wtExist = findByWord(word);
+		if (wtExist.size()==0)
+		{
+			WordTranscription wt= new WordTranscription(ctx,word,trans);
+			wt.save();
+		}
 	}
 
 }
