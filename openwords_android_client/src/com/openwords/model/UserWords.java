055208ace98bb04d6e6d7503ae58b06ce202c8a5
Mayukh Das <maydas@indiana.edu>
2014-07-03 11:55:33 -0400

    add constrctor

--- openwords_android_client/src/com/openwords/model/UserWords.java
+++ openwords_android_client/src/com/openwords/model/UserWords.java
@@ -44,6 +44,28 @@ public class UserWords extends SugarRecord<UserWords> {
 		this.fresh=true;
 	}
 	
+	public UserWords(Context ctx, int connectionId,
+			int wordL2Id,
+			String wordL2,
+			int wordL1Id,
+			String wordL1,
+			int l2Id,
+			String l2Name,
+			String audiocall,
+			boolean fresh)
+			{
+				super(ctx);
+				this.connectionId=connectionId;
+				this.wordLTwoId=wordL2Id;
+				this.wordLTwo=wordL2;
+				this.wordLOneId=wordL1Id;
+				this.wordLOne=wordL1;
+				this.lTwoId=l2Id;
+				this.lTwoName=l2Name;
+				this.audiocall=audiocall;
+				this.fresh=fresh;
+			}
+	
 	public static List<UserWords> findByConnection(int connection_id)
 	{
 		List<UserWords> words = UserWords.find(UserWords.class, "connection_id="+connection_id);
