a23592e489f99be4624159ef8ce9f09fa905b464
Shenshen Han <hanaldo520124@gmail.com>
2014-07-18 13:08:46 -0400

    Add an utility class for creating a progress dialog when user performs a long-running http request action, this dialog is used for prompting user that the app is currently carrying out an important network communication before make the next step, but can be forcelly cancelled by user. For any other http requests which need to be running in background and don't want to block user actions, please then don't use this utility class.

--- openwords_android_client/src/com/openwords/test/ActivityTest.java
+++ openwords_android_client/src/com/openwords/test/ActivityTest.java
@@ -3,7 +3,12 @@ package com.openwords.test;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+import com.loopj.android.http.RequestParams;
 import com.openwords.R;
+import com.openwords.ui.common.DialogForHTTP;
+import com.openwords.util.log.LogUtil;
+import org.apache.http.Header;
 
 /**
  *
@@ -20,6 +25,27 @@ public class ActivityTest extends Activity {
 
             public void onClick(View view) {
 
+                DialogForHTTP d = new DialogForHTTP(ActivityTest.this,
+                        "Downloading Language Pack",
+                        "Retriving something, can be shown in percentage",
+                        "http://156.56.92.178:8888/getWordsList.php",
+                        5000,
+                        new RequestParams("languageId", "1"),
+                        new DialogForHTTP.OnSuccess() {
+
+                            public void onSuccess(int i, Header[] headers, String string) {
+                                Toast.makeText(ActivityTest.this, "onSuccess: " + string, Toast.LENGTH_SHORT).show();
+                                LogUtil.logDeubg(this, "onSuccess: " + string);
+                            }
+                        },
+                        new DialogForHTTP.OnFailure() {
+
+                            public void onFailure(int i, Header[] headers, String string, Throwable thrwbl) {
+                                Toast.makeText(ActivityTest.this, "onFailure: " + thrwbl.toString(), Toast.LENGTH_SHORT).show();
+                                LogUtil.logDeubg(this, "onFailure: " + thrwbl.toString());
+                            }
+                        });
+                d.start();
             }
         });
     }
