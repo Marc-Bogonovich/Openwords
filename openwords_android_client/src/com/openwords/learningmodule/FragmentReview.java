58355add06cc91a140846e221ffbcabf621f45c6
Jianqing Guan <gjqvictory888@gmail.com>
2014-07-22 14:51:57 -0400

    Adding breadcrumbs to Review page

--- openwords_android_client/src/com/openwords/learningmodule/FragmentReview.java
+++ openwords_android_client/src/com/openwords/learningmodule/FragmentReview.java
@@ -8,7 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.openwords.R;
@@ -17,6 +20,7 @@ import com.openwords.model.LeafCardSelfEval;
 import com.openwords.tts.Speak;
 import com.openwords.util.TimeConvertor;
 import com.openwords.util.log.LogUtil;
+import com.openwords.util.preference.OpenwordsSharedPreferences;
 
 @SuppressLint("ValidFragment")
 public class FragmentReview extends Fragment {
@@ -25,6 +29,8 @@ public class FragmentReview extends Fragment {
     private TextView problem, transcription, answer;
     private ImageView audioPlay;
     private LeafCard card;
+    private LinearLayout breadcrumbs;
+    private View myFragmentView;
     public FragmentReview(int cardIndex) {
         this.cardIndex = cardIndex;
     }
@@ -40,16 +46,16 @@ public class FragmentReview extends Fragment {
         super.onCreate(savedInstanceState);
         LogUtil.logDeubg(this, "onCreateView for card: " + cardIndex);
 
-        View myFragmentView = inflater.inflate(R.layout.fragment_review, container, false);
+        myFragmentView = inflater.inflate(R.layout.fragment_review, container, false);
         Log.e("size",Integer.toString(ActivityReview.getCardsPool().size()));
         card = ActivityReview.getCardsPool().get(this.cardIndex);
 
         problem = (TextView) myFragmentView.findViewById(R.id.review_TextView_question);
         transcription = (TextView) myFragmentView.findViewById(R.id.review_TextView_transcription);
         answer = (TextView) myFragmentView.findViewById(R.id.review_TextView_answer);
-     
+        
         audioPlay = (ImageView) myFragmentView.findViewById(R.id.review_ImageView_audioPlay);
-
+        makeBreadCrumbs();
         problem.setText(card.getWordLang2());
         answer.setText(card.getWordLang1());
         transcription.setText(card.getTranscription());
@@ -77,7 +83,23 @@ public class FragmentReview extends Fragment {
                 ActivityReview.getInstance().getPager().setCurrentItem(cardIndex + 1, true);
             }
         });
-
+        
         return myFragmentView;
+    }
+    
+    private void makeBreadCrumbs() {
+    	breadcrumbs = (LinearLayout) myFragmentView.findViewById(R.id.review_LinearLayout_breadcrumbs);
+    	int size = OpenwordsSharedPreferences.getLeafCardSize();
+    	for(int i=0;i<size;i++) {
+    		ImageView crumb = new ImageView(this.getActivity().getApplicationContext());
+    		
+    		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+    		            0,
+    		            LinearLayout.LayoutParams.WRAP_CONTENT, 1);
+    		
+    		crumb.setImageResource(R.drawable.ic_learning_module_breadcrumb_normal);
+    		if(i==cardIndex) crumb.setImageResource(R.drawable.ic_learning_module_breadcrumb_large);
+    		breadcrumbs.addView(crumb, i , params);
+    	}
     }
 }
