aefc3828a1b57984589b591dbead6985b291d888
Shenshen Han <hanaldo520124@gmail.com>
2014-08-28 11:24:18 -0400

    Fix the string comparison for Type Evaluation.

--- openwords_android_client/src/com/openwords/learningmodule/FragmentTypeEval.java
+++ openwords_android_client/src/com/openwords/learningmodule/FragmentTypeEval.java
@@ -1,6 +1,5 @@
 package com.openwords.learningmodule;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -9,40 +8,32 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.openwords.R;
-import com.openwords.model.LeafCardSelfEval;
 import com.openwords.model.LeafCardTypeEval;
-import com.openwords.tts.Speak;
 import com.openwords.util.TimeConvertor;
 import com.openwords.util.WordComparsion;
 import com.openwords.util.log.LogUtil;
-import com.openwords.util.preference.OpenwordsSharedPreferences;
 
-@SuppressWarnings("unused")
-@SuppressLint({ "ValidFragment", "DefaultLocale" })
 public class FragmentTypeEval extends Fragment {
 
     private final int cardIndex;
-	private EditText userInput;
-	private TextView answer;
-	private TextView question;
-	private TextView transcription;
-	private ImageView checkButton;
-	private ImageView audioPlayButton;
-	private ImageView status;
-	private final double CUTOFF = 0.75f;
-	private LeafCardTypeEval card;
+    private EditText userInput;
+    private TextView answer;
+    private TextView question;
+    private TextView transcription;
+    private ImageView checkButton;
+    private ImageView audioPlayButton;
+    private ImageView status;
+    private final double CUTOFF = 0.75f;
+    private LeafCardTypeEval card;
     private LinearLayout breadcrumbs;
     private View myFragmentView;
-	
-    @SuppressLint("ValidFragment")
-	public FragmentTypeEval(int cardIndex) {
+
+    public FragmentTypeEval(int cardIndex) {
         this.cardIndex = cardIndex;
     }
 
@@ -61,102 +52,112 @@ public class FragmentTypeEval extends Fragment {
         card = ActivityTypeEval.getCardsPool().get(this.cardIndex);
 
         answer = (TextView) myFragmentView.findViewById(R.id.typeEvaluate_TextView_answer);
-		question = (TextView) myFragmentView.findViewById(R.id.typeEvaluate_TextView_question);
-		transcription = (TextView) myFragmentView.findViewById(R.id.typeEvaluate_TextView_transcription);
-		userInput = (EditText) myFragmentView.findViewById(R.id.typeEvaluate_EditText_input);
-		checkButton = (ImageView) myFragmentView.findViewById(R.id.typeEvaluate_ImageView_checkButton);
-		status = (ImageView) myFragmentView.findViewById(R.id.typeEvaluate_ImageView_status);
-		audioPlayButton = (ImageView) myFragmentView.findViewById(R.id.typeEvaluate_ImageView_audioPlay);
-		setInterfaceView();
-		//makeBreadCrumbs();
+        question = (TextView) myFragmentView.findViewById(R.id.typeEvaluate_TextView_question);
+        transcription = (TextView) myFragmentView.findViewById(R.id.typeEvaluate_TextView_transcription);
+        userInput = (EditText) myFragmentView.findViewById(R.id.typeEvaluate_EditText_input);
+        checkButton = (ImageView) myFragmentView.findViewById(R.id.typeEvaluate_ImageView_checkButton);
+        status = (ImageView) myFragmentView.findViewById(R.id.typeEvaluate_ImageView_status);
+        audioPlayButton = (ImageView) myFragmentView.findViewById(R.id.typeEvaluate_ImageView_audioPlay);
+        setInterfaceView();
+        //makeBreadCrumbs();
         card.setLastTime(TimeConvertor.getUnixTime());
-		answer.setVisibility(View.INVISIBLE);
-		if(card.getAudioURL()==null) {
-			audioPlayButton.setImageResource(R.drawable.ic_self_evaluate_audio_null);
+        answer.setVisibility(View.INVISIBLE);
+        if (card.getAudioURL() == null) {
+            audioPlayButton.setImageResource(R.drawable.ic_self_evaluate_audio_null);
         } else {
-        	audioPlayButton.setOnClickListener(new View.OnClickListener() {
+            audioPlayButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View view) {
                     //Speak.getInstance(null).speak(card.getWordLang2());
                 }
             });
         }
-		
-		userInput.addTextChangedListener(new TextWatcher(){
-			public void beforeTextChanged(CharSequence s, int start, int count, int after){}
-	        public void afterTextChanged(Editable s){}
-			@SuppressLint("DefaultLocale") @Override
-			public void onTextChanged(CharSequence s, int start, int before, int count) {
-				 {
-						Integer userChoice = 0;
-						
-						if(userInput!=null) {
-							status.setImageResource(R.drawable.ic_learning_module_null);
-							//user choice 0--null 1--wrong 2--close 3--correct
-							String userInputString = userInput.getText().toString().toLowerCase();
-							double similarity = WordComparsion.similarity(userInputString, card.getWordLang1());
-							if(userInputString.equals(card.getWordLang1())) {
-								status.setImageResource(R.drawable.ic_learning_module_correct);
-								card.setUserInput(userInputString);
-								userChoice = 3;
-								Handler mHandler = new Handler();
-								mHandler.postDelayed(new Runnable() {
-						            public void run() {
-						            	ActivityTypeEval.getInstance().getPager().setCurrentItem(cardIndex + 1, true);
-						            }
-						        }, 3000);
-						} else {
-							answer.setVisibility(View.INVISIBLE);
-							status.setImageResource(R.drawable.ic_learning_module_null);
-							userChoice = 0;
-							//answer.setVisibility(View.VISIBLE);
-						}
-						card.setUserChoice(userChoice);
 
-					}
-			}
-		}});
-		
-		checkButton.setOnClickListener(new View.OnClickListener() {
-			@SuppressLint("DefaultLocale") @Override
-			public void onClick(View v) {
-				Integer userChoice = 0;
-				
-				if(userInput!=null) {
-					//user choice 0--null 1--wrong 2--close 3--correct
-					String userInputString = userInput.getText().toString().toLowerCase();
-					double similarity = WordComparsion.similarity(userInputString, card.getWordLang1());
-					if(userInputString.equals(card.getWordLang1())) {
-						status.setImageResource(R.drawable.ic_learning_module_correct);
-						card.setUserInput(userInputString);
-						userChoice = 3;
-					} else if(similarity>=CUTOFF) {
-						status.setImageResource(R.drawable.ic_learning_module_close);
-						userChoice = 2;
-						card.setUserInput(userInputString);
-						answer.setVisibility(View.VISIBLE);
-						//if want the status icon becomes null when move forward/backward, change the value of userChoice
-					} else {
-						status.setImageResource(R.drawable.ic_learning_module_incorrect);
-						userChoice = 1;
-						card.setUserInput(userInputString);
-						answer.setVisibility(View.VISIBLE);
-					}
-				} else {
-					status.setImageResource(R.drawable.ic_learning_module_incorrect);
-					userChoice = 0;
-					answer.setVisibility(View.VISIBLE);
-				}
-				card.setUserChoice(userChoice);
-				
-				Handler mHandler = new Handler();
-				mHandler.postDelayed(new Runnable() {
-		            public void run() {
-		            	ActivityTypeEval.getInstance().getPager().setCurrentItem(cardIndex + 1, true);
-		            }
-		        }, 3000);
-				
-			}
-		});
+        userInput.addTextChangedListener(new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                {
+                    int userChoice;
+
+                    if (userInput != null) {
+                        status.setImageResource(R.drawable.ic_learning_module_null);
+                        //user choice 0--null 1--wrong 2--close 3--correct
+                        String userInputString = userInput.getText().toString().trim();
+                        String correctString = card.getWordLang1().trim();
+
+                        double similarity = WordComparsion.similarity(userInputString, card.getWordLang1());
+                        if (userInputString.equalsIgnoreCase(correctString)) {
+                            status.setImageResource(R.drawable.ic_learning_module_correct);
+                            card.setUserInput(userInputString);
+                            userChoice = 3;
+                            Handler mHandler = new Handler();
+                            mHandler.postDelayed(new Runnable() {
+                                public void run() {
+                                    ActivityTypeEval.getInstance().getPager().setCurrentItem(cardIndex + 1, true);
+                                }
+                            }, 3000);
+                        } else {
+                            answer.setVisibility(View.INVISIBLE);
+                            status.setImageResource(R.drawable.ic_learning_module_null);
+                            userChoice = 0;
+                            //answer.setVisibility(View.VISIBLE);
+                        }
+                        card.setUserChoice(userChoice);
+
+                    }
+                }
+            }
+        });
+
+        checkButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int userChoice;
+
+                if (userInput != null) {
+                    //user choice 0--null 1--wrong 2--close 3--correct
+                    String userInputString = userInput.getText().toString().trim();
+                    String correctString = card.getWordLang1().trim();
+
+                    double similarity = WordComparsion.similarity(userInputString, card.getWordLang1());
+                    if (userInputString.equalsIgnoreCase(correctString)) {
+                        status.setImageResource(R.drawable.ic_learning_module_correct);
+                        card.setUserInput(userInputString);
+                        userChoice = 3;
+                    } else if (similarity >= CUTOFF) {
+                        status.setImageResource(R.drawable.ic_learning_module_close);
+                        userChoice = 2;
+                        card.setUserInput(userInputString);
+                        answer.setVisibility(View.VISIBLE);
+                        //if want the status icon becomes null when move forward/backward, change the value of userChoice
+                    } else {
+                        status.setImageResource(R.drawable.ic_learning_module_incorrect);
+                        userChoice = 1;
+                        card.setUserInput(userInputString);
+                        answer.setVisibility(View.VISIBLE);
+                    }
+                } else {
+                    status.setImageResource(R.drawable.ic_learning_module_incorrect);
+                    userChoice = 0;
+                    answer.setVisibility(View.VISIBLE);
+                }
+                card.setUserChoice(userChoice);
+
+                Handler mHandler = new Handler();
+                mHandler.postDelayed(new Runnable() {
+                    public void run() {
+                        ActivityTypeEval.getInstance().getPager().setCurrentItem(cardIndex + 1, true);
+                    }
+                }, 3000);
+
+            }
+        });
 
         audioPlayButton.setOnClickListener(new View.OnClickListener() {
 
@@ -166,32 +167,32 @@ public class FragmentTypeEval extends Fragment {
         });
         return myFragmentView;
     }
-    
+
     private void setInterfaceView() {
-    	Integer userChoice = card.getUserChoice();
-    	transcription.setText(card.getTranscription());
-    	question.setText(card.getWordLang2());
-    	answer.setText(card.getWordLang1());
-    	if(userChoice.equals(0)) {
-			status.setImageResource(R.drawable.ic_learning_module_null);
-			userInput.setText("");
-			answer.setVisibility(View.INVISIBLE);
-		} else if (userChoice.equals(3)) {
-			status.setImageResource(R.drawable.ic_learning_module_correct);
-			userInput.setText("");	
-			answer.setVisibility(View.INVISIBLE);
-		} else if (userChoice.equals(2)) {
-			status.setImageResource(R.drawable.ic_learning_module_close);
-			userInput.setText("");
-			answer.setVisibility(View.VISIBLE);
-		} else {
-			status.setImageResource(R.drawable.ic_learning_module_incorrect);
-			userInput.setText("");
-			answer.setVisibility(View.VISIBLE);
-		}
-		
-	}
-    
+        Integer userChoice = card.getUserChoice();
+        transcription.setText(card.getTranscription());
+        question.setText(card.getWordLang2());
+        answer.setText(card.getWordLang1());
+        if (userChoice.equals(0)) {
+            status.setImageResource(R.drawable.ic_learning_module_null);
+            userInput.setText("");
+            answer.setVisibility(View.INVISIBLE);
+        } else if (userChoice.equals(3)) {
+            status.setImageResource(R.drawable.ic_learning_module_correct);
+            userInput.setText("");
+            answer.setVisibility(View.INVISIBLE);
+        } else if (userChoice.equals(2)) {
+            status.setImageResource(R.drawable.ic_learning_module_close);
+            userInput.setText("");
+            answer.setVisibility(View.VISIBLE);
+        } else {
+            status.setImageResource(R.drawable.ic_learning_module_incorrect);
+            userInput.setText("");
+            answer.setVisibility(View.VISIBLE);
+        }
+
+    }
+
 //    private void makeBreadCrumbs() {
 //    	breadcrumbs = (LinearLayout) myFragmentView.findViewById(R.id.review_LinearLayout_breadcrumbs);
 //    	int size = OpenwordsSharedPreferences.getLeafCardSize();
