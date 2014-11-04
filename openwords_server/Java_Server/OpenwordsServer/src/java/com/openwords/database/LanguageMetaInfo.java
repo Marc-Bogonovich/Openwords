e868cc224e41f612d3f7389c4bff50f3ac170bf2
Shenshen Han <hanaldo520124@gmail.com>
2014-11-04 12:55:24 -0500

    Make two classes for the xml meta-info strings.

---
+++ openwords_server/Java_Server/OpenwordsServer/src/java/com/openwords/database/LanguageMetaInfo.java
@@ -0,0 +1,18 @@
+package com.openwords.database;
+
+public class LanguageMetaInfo {
+
+    private String wikiCode;
+
+    public LanguageMetaInfo() {
+    }
+
+    public String getWikiCode() {
+        return wikiCode;
+    }
+
+    public void setWikiCode(String wikiCode) {
+        this.wikiCode = wikiCode;
+    }
+
+}
---
+++ openwords_server/Java_Server/OpenwordsServer/src/java/com/openwords/database/WordMetaInfo.java
@@ -0,0 +1,61 @@
+package com.openwords.database;
+
+import com.openwords.utils.MyXStream;
+
+public class WordMetaInfo {
+
+    private Integer commRank, popRank;
+    private String commonTranslation, nativeTranslation, nativeForm;
+
+    public WordMetaInfo() {
+    }
+
+    public WordMetaInfo(String commonTranslation, String nativeForm) {
+        this.commonTranslation = commonTranslation;
+        this.nativeForm = nativeForm;
+    }
+
+    public Integer getCommRank() {
+        return commRank;
+    }
+
+    public void setCommRank(Integer commRank) {
+        this.commRank = commRank;
+    }
+
+    public Integer getPopRank() {
+        return popRank;
+    }
+
+    public void setPopRank(Integer popRank) {
+        this.popRank = popRank;
+    }
+
+    public String getCommonTranslation() {
+        return commonTranslation;
+    }
+
+    public void setCommonTranslation(String commonTranslation) {
+        this.commonTranslation = commonTranslation;
+    }
+
+    public String getNativeTranslation() {
+        return nativeTranslation;
+    }
+
+    public void setNativeTranslation(String nativeTranslation) {
+        this.nativeTranslation = nativeTranslation;
+    }
+
+    public String getNativeForm() {
+        return nativeForm;
+    }
+
+    public void setNativeForm(String nativeForm) {
+        this.nativeForm = nativeForm;
+    }
+
+    public String getXmlString() {
+        return MyXStream.toXml("word", WordMetaInfo.class, this);
+    }
+}
