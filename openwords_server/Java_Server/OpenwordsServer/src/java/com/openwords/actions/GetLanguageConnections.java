ee045a7cd4abc51b6764f914a487ee1fbec5adbb
Shenshen Han <hanaldo520124@gmail.com>
2014-11-10 13:42:45 -0500

    Rename to service "GetLanguageConnections".

---
+++ openwords_server/Java_Server/OpenwordsServer/src/java/com/openwords/actions/GetLanguageConnections.java
@@ -0,0 +1,67 @@
+package com.openwords.actions;
+
+import static com.opensymphony.xwork2.Action.SUCCESS;
+import com.openwords.database.DatabaseHandler;
+import com.openwords.database.WordConnection;
+import com.openwords.interfaces.MyAction;
+import com.openwords.utils.UtilLog;
+import java.util.List;
+import java.util.Map;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.hibernate.Session;
+
+@ParentPackage("json-default")
+public class GetLanguageConnections extends MyAction {
+
+    private static final long serialVersionUID = 1L;
+    private int langOneId, langTwoId, pageNumber, pageSize;
+    private List<Map<String, Object>> result;
+    private String errorMessage;
+
+    @Action(value = "/getLanguageConnections", results = {
+        @Result(name = SUCCESS, type = "json")
+    })
+    @Override
+    public String execute() throws Exception {
+        Session s = DatabaseHandler.getSession();
+        try {
+            if (pageSize > 100) {
+                throw new Exception("PageSize is too large!");
+            }
+            result = WordConnection.getConnectionsPage(s, langOneId, langTwoId, pageNumber, pageSize);
+        } catch (Exception e) {
+            UtilLog.logWarn(this, e.toString());
+            errorMessage = e.toString();
+        } finally {
+            DatabaseHandler.closeSession(s);
+        }
+        return SUCCESS;
+    }
+
+    public void setLangOneId(int langOneId) {
+        this.langOneId = langOneId;
+    }
+
+    public void setLangTwoId(int langTwoId) {
+        this.langTwoId = langTwoId;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public List<Map<String, Object>> getResult() {
+        return result;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+}
