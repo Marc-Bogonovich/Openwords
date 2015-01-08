5306029dbe4f74ff02322418c8c5ad3f696f3a15
Shenshen Han <hanaldo520124@gmail.com>
2015-01-08 17:05:00 -0500

    Add a few user services.

---
+++ openwords_server/Java_Server/OpenwordsServer/src/java/com/openwords/actions/CheckAddUser.java
@@ -0,0 +1,83 @@
+package com.openwords.actions;
+
+import static com.opensymphony.xwork2.Action.SUCCESS;
+import com.openwords.database.DatabaseHandler;
+import com.openwords.database.UserInfo;
+import static com.openwords.database.UserInfo.checkEmail;
+import static com.openwords.database.UserInfo.checkUserName;
+import com.openwords.interfaces.MyAction;
+import com.openwords.utils.MyFieldValidation;
+import com.openwords.utils.UtilLog;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.hibernate.Session;
+
+@ParentPackage("json-default")
+public class CheckAddUser extends MyAction {
+
+    private static final long serialVersionUID = 1L;
+    private boolean result;
+    private int userId = -1;
+    private String username, password, email, errorMessage;
+
+    @Action(value = "/addUser", results = {
+        @Result(name = SUCCESS, type = "json"),
+        @Result(name = INPUT, type = "json")
+    })
+    @Override
+    public String execute() throws Exception {
+        UtilLog.logInfo(this, "/addUser");
+        Session s = DatabaseHandler.getSession();
+        try {
+            if (checkUserName(s, username) && checkEmail(s, email)) {
+                userId = UserInfo.addUser(s, username, password, email);
+                result = true;
+            } else {
+                errorMessage = "username or email is already registered.";
+            }
+        } catch (Exception e) {
+            errorMessage = e.toString();
+            UtilLog.logWarn(this, errorMessage);
+        } finally {
+            DatabaseHandler.closeSession(s);
+        }
+        return SUCCESS;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    @Override
+    public void validate() {
+        MyFieldValidation.checkUsernameField(this, username);
+        MyFieldValidation.checkPasswordField(this, password);
+        MyFieldValidation.checkEmailField(this, email);
+    }
+
+    @Override
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
