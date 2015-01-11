e2fb43caf9cb68a612d46978074ff81ebb5f241f
Shenshen Han <hanaldo520124@gmail.com>
2015-01-11 17:26:43 -0500

    Extend GetLanguages service.

--- openwords_server/Java_Server/OpenwordsServer/src/java/com/openwords/actions/languages/GetLanguages.java
+++ openwords_server/Java_Server/OpenwordsServer/src/java/com/openwords/actions/languages/GetLanguages.java
@@ -6,6 +6,7 @@ import static com.opensymphony.xwork2.Action.SUCCESS;
 import com.openwords.database.DatabaseHandler;
 import com.openwords.database.Language;
 import com.openwords.interfaces.MyAction;
+import com.openwords.utils.MyFieldValidation;
 import com.openwords.utils.UtilLog;
 import java.util.List;
 import org.apache.struts2.convention.annotation.Action;
@@ -20,22 +21,34 @@ public class GetLanguages extends MyAction {
     private List<Language> result;
     private String errorMessage;
     private String exclude;
+    private String include;
 
     @Action(value = "/getLanguages", results = {
-        @Result(name = SUCCESS, type = "json")
+        @Result(name = SUCCESS, type = "json"),
+        @Result(name = INPUT, type = "json")
     })
     @Override
     public String execute() throws Exception {
-        UtilLog.logInfo(this, "/getLanguages: " + exclude);
+        UtilLog.logInfo(this, "/getLanguages: exclude->" + exclude + " include->" + include);
         Session s = DatabaseHandler.getSession();
         try {
-            List<Integer> old = new Gson().fromJson(exclude, new TypeToken<List<Integer>>() {
-            }.getType());
-            List<Integer> langIds = Language.getNewLanguageIds(s, old);
-            if (langIds.isEmpty()) {
-                return SUCCESS;
+            if (include == null) {
+                List<Integer> old = new Gson().fromJson(exclude, new TypeToken<List<Integer>>() {
+                }.getType());
+                List<Integer> langIds = Language.getNewLanguageIds(s, old);
+                if (langIds.isEmpty()) {
+                    return SUCCESS;
+                }
+                result = Language.getLanguages(s, langIds);
+            } else {
+                List<Integer> needed = new Gson().fromJson(include, new TypeToken<List<Integer>>() {
+                }.getType());
+                if (needed.isEmpty()) {
+                    return SUCCESS;
+                }
+                result = Language.getLanguages(s, needed);
             }
-            result = Language.getLanguages(s, langIds);
+
         } catch (Exception e) {
             errorMessage = e.toString();
             UtilLog.logWarn(this, errorMessage);
@@ -49,6 +62,10 @@ public class GetLanguages extends MyAction {
         this.exclude = exclude;
     }
 
+    public void setInclude(String include) {
+        this.include = include;
+    }
+
     public String getErrorMessage() {
         return errorMessage;
     }
@@ -58,6 +75,28 @@ public class GetLanguages extends MyAction {
     }
 
     @Override
+    public void validate() {
+        try {
+            if (exclude == null) {
+                if (include.isEmpty()) {
+                    throw new Exception("include is empty");
+                }
+            } else if (include == null) {
+                if (exclude.isEmpty()) {
+                    throw new Exception("exclude is empty");
+                }
+            } else {
+                throw new Exception("both include and exclude are provided");
+            }
+        } catch (Exception e) {
+            setErrorMessage("please either provide excluded ids or included ids, not both");
+            UtilLog.logWarn(MyFieldValidation.class, e.toString());
+            addFieldError("error", "error");
+        }
+    }
+
+    @Override
     public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
     }
 }
