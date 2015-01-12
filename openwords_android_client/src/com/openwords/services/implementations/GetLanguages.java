26d6efcf9a0c5e8b376e9f8593f30c4778e718eb
Shenshen Han <hanaldo520124@gmail.com>
2015-01-11 23:14:28 -0500

    Add GetLearnableLanguages service and GetLanguages service.

---
+++ openwords_android_client/src/com/openwords/services/implementations/GetLanguages.java
@@ -0,0 +1,41 @@
+package com.openwords.services.implementations;
+
+import com.google.gson.Gson;
+import com.loopj.android.http.RequestParams;
+import com.openwords.model.Language;
+import com.openwords.services.interfaces.HttpResultHandler;
+import com.openwords.services.interfaces.HttpServiceRequester;
+import java.util.List;
+
+public class GetLanguages extends HttpServiceRequester implements HttpResultHandler {
+
+    public final String ServiceURL = "http://192.168.1.104:8080/OpenwordsServer/getLanguages";
+
+    private HttpResultHandler resultHandler;
+
+    @Override
+    public void doRequest(RequestParams params, HttpResultHandler resultHandler) {
+        this.resultHandler = resultHandler;
+        request(ServiceURL, params, 0, this);
+    }
+
+    public void hasResult(Object resultObject) {
+        String jsonReply = (String) resultObject;
+        Result r = new Gson().fromJson(jsonReply, Result.class);
+        if (r.result == null) {
+            resultHandler.noResult("no result");
+        } else {
+            resultHandler.hasResult(r.result);
+        }
+    }
+
+    public void noResult(String errorMessage) {
+        resultHandler.noResult(errorMessage);
+    }
+
+    public class Result {
+
+        public List<Language> result;
+        public String errorMessage;
+    }
+}
