f78b3e85e9b311bec13e871cef29d9c45d1cbed5
Shenshen Han <hanaldo520124@gmail.com>
2015-01-17 16:22:15 -0500

    Put most local settings into a pack.

---
+++ openwords_android_client/src/com/openwords/model/LocalSettings.java
@@ -0,0 +1,62 @@
+package com.openwords.model;
+
+import com.openwords.util.localization.LocalLanguage;
+import com.orm.SugarRecord;
+
+public class LocalSettings extends SugarRecord<LocalSettings> {
+
+    private int userId, baseLanguageId;
+    private String username, password, localLanguage;
+    private boolean remember;
+
+    public LocalSettings() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getBaseLanguageId() {
+        return baseLanguageId;
+    }
+
+    public void setBaseLanguageId(int baseLanguageId) {
+        this.baseLanguageId = baseLanguageId;
+    }
+
+    public boolean isRemember() {
+        return remember;
+    }
+
+    public void setRemember(boolean remember) {
+        this.remember = remember;
+    }
+
+    public LocalLanguage getLocalLanguage() {
+        return null;
+    }
+
+    public void setLocalLanguage() {
+
+    }
+}
