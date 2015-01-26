64d205d41f742a8bb30de7a22b953396f47f5924
Shenshen Han <hanaldo520124@gmail.com>
2015-01-26 17:36:49 -0500

    Implement two new interfaces.

--- openwords_android_client/src/com/openwords/services/implementations/GetLearnableLanguages.java
+++ openwords_android_client/src/com/openwords/services/implementations/GetLearnableLanguages.java
@@ -2,21 +2,24 @@ package com.openwords.services.implementations;
 
 import com.google.gson.Gson;
 import com.loopj.android.http.RequestParams;
+import com.openwords.interfaces.HttpResultHandler;
+import com.openwords.interfaces.InterfaceGetLearnableLanguages;
 import static com.openwords.model.DataPool.ServerAddress;
-import com.openwords.services.interfaces.HttpResultHandler;
 import com.openwords.services.interfaces.HttpServiceRequester;
+import com.openwords.services.interfaces.RequestParamsBuilder;
 import java.util.List;
 
-public class GetLearnableLanguages extends HttpServiceRequester implements HttpResultHandler {
+public class GetLearnableLanguages extends HttpServiceRequester implements HttpResultHandler, InterfaceGetLearnableLanguages {
 
-    public final String ServiceURL = "http://" + ServerAddress + "/getLearnableLanguages";
+    public final String ServiceURL = "http://" + ServerAddress + servicePath;
 
     private HttpResultHandler resultHandler;
 
-    @Override
-    public void doRequest(RequestParams params, HttpResultHandler resultHandler) {
-        this.resultHandler = resultHandler;
-        request(ServiceURL, params, 0, this);
+    public void doRequest(int langOneId, HttpResultHandler hrh) {
+        resultHandler = hrh;
+        request(ServiceURL, new RequestParamsBuilder()
+                .addParam("langOneId", String.valueOf(langOneId))
+                .getParams(), 0, this);
     }
 
     public void hasResult(Object resultObject) {
@@ -31,6 +34,19 @@ public class GetLearnableLanguages extends HttpServiceRequester implements HttpR
 
     public void noResult(String errorMessage) {
         resultHandler.noResult(errorMessage);
+    }
+
+    public void setLangOneId(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<Integer> getResult() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void doRequest(RequestParams params, HttpResultHandler resultHandler) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public class Result {
--- openwords_android_client/src/com/openwords/services/implementations/GetUserLanguages.java
+++ openwords_android_client/src/com/openwords/services/implementations/GetUserLanguages.java
@@ -2,16 +2,26 @@ package com.openwords.services.implementations;
 
 import com.google.gson.Gson;
 import com.loopj.android.http.RequestParams;
+import com.openwords.interfaces.HttpResultHandler;
+import com.openwords.interfaces.InterfaceGetUserLanguages;
 import static com.openwords.model.DataPool.ServerAddress;
-import com.openwords.services.interfaces.HttpResultHandler;
 import com.openwords.services.interfaces.HttpServiceRequester;
+import com.openwords.services.interfaces.RequestParamsBuilder;
 import java.util.List;
 
-public class GetUserLanguages extends HttpServiceRequester implements HttpResultHandler {
+public class GetUserLanguages extends HttpServiceRequester implements HttpResultHandler, InterfaceGetUserLanguages {
 
-    public final String ServiceURL = "http://" + ServerAddress + "/getUserLanguages";
+    public final String ServiceURL = "http://" + ServerAddress + servicePath;
 
     private HttpResultHandler resultHandler;
+
+    public void doRequest(int userId, int langOneId, HttpResultHandler hrh) {
+        resultHandler = hrh;
+        request(ServiceURL, new RequestParamsBuilder()
+                .addParam("userId", String.valueOf(userId))
+                .addParam("langOneId", String.valueOf(langOneId))
+                .getParams(), 0, this);
+    }
 
     @Override
     public void doRequest(RequestParams params, HttpResultHandler resultHandler) {
@@ -31,6 +41,18 @@ public class GetUserLanguages extends HttpServiceRequester implements HttpResult
 
     public void noResult(String errorMessage) {
         resultHandler.noResult(errorMessage);
+    }
+
+    public void setUserId(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setLangOneId(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<Integer> getResult() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public class Result {
