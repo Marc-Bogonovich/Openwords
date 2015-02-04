c6ed6959967f81d1029cfea8fc54aee957cc4f4e
Shenshen Han <hanaldo520124@gmail.com>
2015-02-04 18:10:10 -0500

    Previous LM type was too complicated.

--- openwords_android_client/src/com/openwords/learningmodule/InterfaceLearningModule.java
+++ openwords_android_client/src/com/openwords/learningmodule/InterfaceLearningModule.java
@@ -7,4 +7,7 @@ package com.openwords.learningmodule;
 public interface InterfaceLearningModule {
 
     public final static int Learning_Type_Review = 1;
+    public final static int Learning_Type_Self = 2;
+    public final static int Learning_Type_Type = 3;
+    public final static int Learning_Type_Hearing = 4;
 }
