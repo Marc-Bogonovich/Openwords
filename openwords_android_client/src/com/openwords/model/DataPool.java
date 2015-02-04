576b39c0f41feeb588fcb8a722c744318646713f
Shenshen Han <hanaldo520124@gmail.com>
2015-02-04 17:43:57 -0500

    Put temp data for Learning Module in DataPool, much more simple and clear.

--- openwords_android_client/src/com/openwords/model/DataPool.java
+++ openwords_android_client/src/com/openwords/model/DataPool.java
@@ -1,5 +1,8 @@
 package com.openwords.model;
 
+import com.openwords.learningmodule.LearningModuleType;
+import java.util.List;
+
 /**
  * This class is used for referring all the static variables centrally.
  *
@@ -10,6 +13,10 @@ public class DataPool {
     public final static String ServerAddress = "192.168.1.109:8080/OpenwordsServer";
     public static boolean DoRegistration = false;
     public static boolean OffLine = false;
+    public static LearningModuleType LmType;
+    public static List<WordConnection> LmPool;
+    public static int LmCurrentCard = -1;
+    public static boolean LmReverseNav;
 
     private DataPool() {
     }
