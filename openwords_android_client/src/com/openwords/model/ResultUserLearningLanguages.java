46ca6c75f2ce45ca9e6da0acb720c238a4df0c35
Shenshen Han <hanaldo520124@gmail.com>
2015-02-05 16:48:49 -0500

    Start to add specific callback.

---
+++ openwords_android_client/src/com/openwords/model/ResultUserLearningLanguages.java
@@ -0,0 +1,12 @@
+package com.openwords.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author hanaldo
+ */
+public interface ResultUserLearningLanguages {
+
+    public void result(List<UserLearningLanguages> result);
+}
