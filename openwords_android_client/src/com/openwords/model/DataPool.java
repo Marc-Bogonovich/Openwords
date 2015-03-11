7118007665ea301dd39b0b6148337609d89a80f4
Shenshen Han <hanaldo520124@gmail.com>
2015-03-11 17:50:47 -0400

    Refacor DataPool for LM.

--- openwords_android_client/src/com/openwords/model/DataPool.java
+++ openwords_android_client/src/com/openwords/model/DataPool.java
@@ -1,23 +1,58 @@
 package com.openwords.model;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
-/**
- * This class is used for referring all the static variables centrally.
- *
- * @author hanaldo
- */
 public class DataPool {
 
     public final static String ServerAddress = "192.168.1.109:8080/OpenwordsServer";
     public static boolean DoRegistration = false;
     public static boolean OffLine = false;
     public static int LmType;
-    public static List<WordConnection> LmPool;
+    private static List<WordConnection> LmPool = new LinkedList<WordConnection>();
+    private static Map<Integer, Performance> LmPerformance = new HashMap<Integer, Performance>(10);
     public static int LmCurrentCard = -1;
     public static boolean LmReverseNav;
     public static int LmLearningLang = -1;
 
+    public static int getPoolSize() {
+        return LmPool.size();
+    }
+
+    public static WordConnection getWordConnection(int index) {
+        return LmPool.get(index);
+    }
+
+    public static Performance getPerformance(int wordConectionId) {
+        return LmPerformance.get(wordConectionId);
+    }
+
+    public static Collection<Performance> getAllPerformance(boolean applyLocalVersion) {
+        if (applyLocalVersion) {
+            for (Performance perf : LmPerformance.values()) {
+                perf.version = perf.tempVersion;
+            }
+        }
+        return LmPerformance.values();
+    }
+
+    public static void clearLmPool() {
+        LmPool.clear();
+        LmPerformance.clear();
+    }
+
+    public static void addLmPool(List<WordConnection> wc, List<Performance> performance) {
+        clearLmPool();
+        LmPool.addAll(wc);
+        for (Performance perf : performance) {
+            LmPerformance.put(perf.wordConnectionId, perf);
+        }
+    }
+
     private DataPool() {
     }
+
 }
