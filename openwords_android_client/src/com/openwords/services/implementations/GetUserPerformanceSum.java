3a147bd8e4d8aafc40ae7b0e7cfbc71e9f100d54
Shenshen Han <hanaldo520124@gmail.com>
2015-04-02 12:37:32 -0400

    Add handler for service getUserPerformanceSum.

---
+++ openwords_android_client/src/com/openwords/services/implementations/GetUserPerformanceSum.java
@@ -0,0 +1,43 @@
+package com.openwords.services.implementations;
+
+import com.google.gson.Gson;
+import static com.openwords.model.DataPool.ServerAddress;
+import com.openwords.services.interfaces.HttpResultHandler;
+import com.openwords.services.interfaces.HttpServiceRequester;
+import com.openwords.services.interfaces.RequestParamsBuilder;
+
+public class GetUserPerformanceSum extends HttpServiceRequester implements HttpResultHandler {
+
+    public final String ServiceURL = "http://" + ServerAddress + "/getUserPerformanceSum";
+
+    private HttpResultHandler resultHandler;
+
+    public void doRequest(int userId, int baseLang, int learningLang, HttpResultHandler hrh) {
+        resultHandler = hrh;
+        request(ServiceURL, new RequestParamsBuilder()
+                .addParam("userId", String.valueOf(userId))
+                .addParam("baseLang", String.valueOf(baseLang))
+                .addParam("learningLang", String.valueOf(learningLang))
+                .getParams(), 0, this);
+    }
+
+    public void hasResult(Object resultObject) {
+        String jsonReply = (String) resultObject;
+        Result r = new Gson().fromJson(jsonReply, Result.class);
+        if (r.errorMessage != null) {
+            resultHandler.noResult(r.errorMessage);
+        } else {
+            resultHandler.hasResult(r);
+        }
+    }
+
+    public void noResult(String errorMessage) {
+        resultHandler.noResult(errorMessage);
+    }
+
+    public class Result {
+
+        public int totalGood, total, totalVersion;
+        public String errorMessage;
+    }
+}
