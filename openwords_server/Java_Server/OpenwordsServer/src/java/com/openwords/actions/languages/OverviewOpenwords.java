fac156ca7546ea3da90b12b77e893d556ad86a83
Shenshen Han <hanaldo520124@gmail.com>
2015-09-03 21:23:56 -0400

    Add total sounds for overview.

--- openwords_server/Java_Server/OpenwordsServer/src/java/com/openwords/actions/languages/OverviewOpenwords.java
+++ openwords_server/Java_Server/OpenwordsServer/src/java/com/openwords/actions/languages/OverviewOpenwords.java
@@ -16,12 +16,13 @@ import org.hibernate.Session;
 
 @ParentPackage("json-default")
 public class OverviewOpenwords extends MyAction {
-    
+
     private static final long serialVersionUID = 1L;
     private static long lastRequest;
     private static List<String[]> result;
     private String errorMessage;
-    
+    private static int totalWords, totalSounds;
+
     @Action(value = "/overviewOpenwords", results = {
         @Result(name = SUCCESS, type = "json")
     })
@@ -34,13 +35,13 @@ public class OverviewOpenwords extends MyAction {
             UtilLog.logInfo(this, "reuse because " + diff + " seconds");
             return SUCCESS;
         }
-        
+
         Session s = DatabaseHandler.getSession();
         try {
             List<Language> langs = Language.getAllLanguages(s);
             result = new LinkedList<>();
-            result.add(new String[]{"ALL", String.valueOf(Word.countLanguageWord(s, -1))});
-            result.add(new String[]{"SOUND", String.valueOf(WordAudio.countAll(s))});
+            totalWords = Word.countLanguageWord(s, -1);
+            totalSounds = WordAudio.countAll(s);
             for (Language lang : langs) {
                 result.add(new String[]{lang.getName(), String.valueOf(Word.countLanguageWord(s, lang.getLangId())), lang.getCode(), String.valueOf(lang.getLangId())});
             }
@@ -53,15 +54,23 @@ public class OverviewOpenwords extends MyAction {
         }
         return SUCCESS;
     }
-    
+
     public List<String[]> getResult() {
         return result;
     }
-    
+
     public String getErrorMessage() {
         return errorMessage;
     }
-    
+
+    public int getTotalWords() {
+        return totalWords;
+    }
+
+    public int getTotalSounds() {
+        return totalSounds;
+    }
+
     @Override
     public void setErrorMessage(String errorMessage) {
     }
