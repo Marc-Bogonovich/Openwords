20712f99338078147c328099f1983622894a77af
Shenshen Han <hanaldo520124@gmail.com>
2015-09-03 21:24:47 -0400

    Fix json return.

--- openwords_server/Java_Server/OpenwordsServer/web/ng/control_root.js
+++ openwords_server/Java_Server/OpenwordsServer/web/ng/control_root.js
@@ -3,9 +3,8 @@ function RootControl($scope, $http) {
     $scope.loadOverview = function() {
         $http({method: "post", url: "overviewOpenwords"}).success(function(data) {
             $scope.langList = data.result;
-            $scope.totalWords = $scope.langList[0][1];
-            $scope.totalSounds = $scope.langList[1][1];
-            $scope.langList = $scope.langList.slice(2, $scope.langList.length);
+            $scope.totalWords = data.totalWords;
+            $scope.totalSounds = data.totalSounds;
         });
     };
 
