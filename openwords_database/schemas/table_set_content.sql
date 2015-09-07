8715e0edf486a8fad2cf7258e2b317d75382f9a4
Shenshen Han <hanaldo520124@gmail.com>
2015-09-07 19:52:08 -0400

    Update set_content schema, removing auto_id.

--- openwords_database/schemas/table_set_content.sql
+++ openwords_database/schemas/table_set_content.sql
@@ -9,7 +9,6 @@
 --
 
 CREATE TABLE `set_content` (
-`auto_id` bigint(20) NOT NULL,
   `set_id` bigint(20) NOT NULL,
   `word_connection_id` bigint(20) NOT NULL,
   `direction` int(11) NOT NULL,
@@ -24,14 +23,4 @@ CREATE TABLE `set_content` (
 -- Indexes for table `set_content`
 --
 ALTER TABLE `set_content`
- ADD PRIMARY KEY (`auto_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `set_content`
---
-ALTER TABLE `set_content`
-MODIFY `auto_id` bigint(20) NOT NULL AUTO_INCREMENT;
+ ADD PRIMARY KEY (`set_id`,`word_connection_id`);
