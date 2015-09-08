cdbabf0ac3f09194537da46a7418521db9696787
Shenshen Han <hanaldo520124@gmail.com>
2015-09-08 15:52:35 -0400

    Add service GetSetItems.

---
+++ openwords_server/Java_Server/OpenwordsServer/src/java/com/openwords/actions/sets/GetSetItems.java
@@ -0,0 +1,66 @@
+package com.openwords.actions.sets;
+
+import static com.opensymphony.xwork2.Action.SUCCESS;
+import com.openwords.database.DatabaseHandler;
+import com.openwords.database.SetInfo;
+import com.openwords.database.SetItem;
+import com.openwords.interfaces.MyAction;
+import com.openwords.utils.UtilLog;
+import java.util.List;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.hibernate.Session;
+
+@ParentPackage("json-default")
+public class GetSetItems extends MyAction {
+
+    private static final long serialVersionUID = 1L;
+    private String errorMessage;
+    private long setId, userId;
+    private List<SetItem> itemsResult;
+
+    @Action(value = "/getSetItems", results = {
+        @Result(name = SUCCESS, type = "json")
+    })
+    @Override
+    public String execute() throws Exception {
+        UtilLog.logInfo(this, "/getSetItems: " + setId);
+        Session s = DatabaseHandler.getSession();
+        try {
+            SetInfo info = SetInfo.getSetInfo(s, setId);
+            if (info.getUserId() != userId) {
+                throw new Exception("set information is not correct");
+            }
+
+            itemsResult = SetItem.getSetItems(s, setId);
+
+        } catch (Exception e) {
+            errorMessage = e.toString();
+            UtilLog.logWarn(this, errorMessage);
+        } finally {
+            DatabaseHandler.closeSession(s);
+        }
+        return SUCCESS;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public void setSetId(long setId) {
+        this.setId = setId;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public List<SetItem> getItemsResult() {
+        return itemsResult;
+    }
+
+    @Override
+    public void setErrorMessage(String errorMessage) {
+    }
+}
