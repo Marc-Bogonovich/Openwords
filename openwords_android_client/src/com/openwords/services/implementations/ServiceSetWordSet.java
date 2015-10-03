0ecb4923fb782ab4a8704a406e285bded1908577
Shenshen Han <hanaldo520124@gmail.com>
2015-10-03 01:31:58 -0400

    Add client handler for service SetWordSet.

---
+++ openwords_android_client/src/com/openwords/services/implementations/ServiceSetWordSet.java
@@ -0,0 +1,48 @@
+package com.openwords.services.implementations;
+
+import com.google.gson.Gson;
+import static com.openwords.model.DataPool.ServerAddress;
+import static com.openwords.model.DataPool.ServiceProtocol;
+import com.openwords.model.SetInfo;
+import com.openwords.services.interfaces.HttpResultHandler;
+import com.openwords.services.interfaces.HttpServiceRequester;
+import com.openwords.services.interfaces.RequestParamsBuilder;
+
+public class ServiceSetWordSet extends HttpServiceRequester implements HttpResultHandler {
+
+    public final String ServiceURL = ServiceProtocol + ServerAddress + "/setWordSet";
+
+    private HttpResultHandler resultHandler;
+
+    public void doRequest(long setId, long userId, int nlang, int llang, String name, HttpResultHandler resultHandler) {
+        this.resultHandler = resultHandler;
+        request(ServiceURL,
+                new RequestParamsBuilder()
+                .addParam("setId", String.valueOf(setId))
+                .addParam("userId", String.valueOf(userId))
+                .addParam("nlang", String.valueOf(nlang))
+                .addParam("llang", String.valueOf(llang))
+                .addParam("name", name)
+                .getParams(), 30 * 1000, this);
+    }
+
+    public void hasResult(Object resultObject) {
+        String jsonReply = (String) resultObject;
+        Result r = new Gson().fromJson(jsonReply, Result.class);
+        if (r.setResult == null) {
+            resultHandler.noResult("cannot update set");
+        } else {
+            resultHandler.hasResult(r);
+        }
+    }
+
+    public void noResult(String errorMessage) {
+        resultHandler.noResult(errorMessage);
+    }
+
+    public class Result {
+
+        public SetInfo setResult;
+        public String errorMessage;
+    }
+}
