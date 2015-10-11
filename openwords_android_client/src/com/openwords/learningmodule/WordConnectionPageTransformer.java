f135123019f695bc0e778a132a09fa8017bd6abc
Shenshen Han <hanaldo520124@gmail.com>
2015-10-11 15:05:31 -0400

    Remove the page when a page is no longer fully visible. i.e. the "position" parameter reports a full -1 or 1.

--- openwords_android_client/src/com/openwords/learningmodule/WordConnectionPageTransformer.java
+++ openwords_android_client/src/com/openwords/learningmodule/WordConnectionPageTransformer.java
@@ -27,9 +27,10 @@ public class WordConnectionPageTransformer implements ViewPager.PageTransformer 
     private void doTransformPage(View view, float position) {
         int pageWidth = view.getWidth();
 
-        if (position < -1) { // [-Infinity,-1)
+        if (position <= -1) { // [-Infinity,-1)
             // This page is way off-screen to the left.
             view.setAlpha(0);
+            view.setTranslationX(0);
 
         } else if (position <= 0) { // [-1,0]
             // Use the default slide transition when moving to the left page
@@ -38,7 +39,7 @@ public class WordConnectionPageTransformer implements ViewPager.PageTransformer 
             view.setScaleX(1);
             view.setScaleY(1);
 
-        } else if (position <= 1) { // (0,1]
+        } else if (position < 1) { // (0,1]
             // Fade the page out.
             view.setAlpha(1 - position);
 
@@ -54,6 +55,7 @@ public class WordConnectionPageTransformer implements ViewPager.PageTransformer 
         } else { // (1,+Infinity]
             // This page is way off-screen to the right.
             view.setAlpha(0);
+            view.setTranslationX(0);
         }
     }
 
