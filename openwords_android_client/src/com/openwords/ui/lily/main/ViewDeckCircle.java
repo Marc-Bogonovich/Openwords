b2a8b74df5d9a957da51c2c82f4546a655868303
Shenshen Han <hanaldo520124@gmail.com>
2015-12-10 17:52:08 -0500

    Draw icon.

--- openwords_android_client/src/com/openwords/ui/lily/main/ViewDeckCircle.java
+++ openwords_android_client/src/com/openwords/ui/lily/main/ViewDeckCircle.java
@@ -1,18 +1,25 @@
 package com.openwords.ui.lily.main;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.ColorFilter;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class ViewDeckCircle extends View {
 
-    private Paint shadePaint, markerPaint;
-    private float centerX, centerY, radius, textX, textY;
-    private int color, alpha, markerColor, markerAlpha;
-    private boolean drawMarker;
+    private final float iconScale = 0.4f;
+    private Bitmap icon;
+    private Bitmap drawIcon;
+    private Paint shadePaint, textPaint, iconPaint;
+    private float centerX, centerY, radius, textX, textY, iconX, iconY;
+    private int color, alpha, iconColor, textColor, textAlpha, iconResource;
     private String text;
 
     public ViewDeckCircle(Context context) {
@@ -27,23 +34,41 @@ public class ViewDeckCircle extends View {
         super(context, attrs, defStyle);
     }
 
-    public ViewDeckCircle(Context context, int color, int alpha, boolean drawMarker, int markerColor, int markerAlpha) {
+    public ViewDeckCircle(Context context, int color, int alpha, int iconResource, int iconColor, int textColor, int markerAlpha) {
         super(context);
         this.color = color;
         this.alpha = alpha;
-        this.markerColor = markerColor;
-        this.markerAlpha = markerAlpha;
-        this.drawMarker = drawMarker;
+        this.iconColor = iconColor;
+        this.textColor = textColor;
+        this.textAlpha = markerAlpha;
+        this.iconResource = iconResource;
         makePaints();
+        loadImages(iconResource);
     }
 
-    public void config(int color, int alpha, boolean drawMarker, int markerColor, int markerAlpha) {
+    public void config(int color, int alpha, int iconResource, int iconColor, int textColor, int textAlpha) {
         this.color = color;
         this.alpha = alpha;
-        this.markerColor = markerColor;
-        this.markerAlpha = markerAlpha;
-        this.drawMarker = drawMarker;
+        this.iconColor = iconColor;
+        this.textColor = textColor;
+        this.textAlpha = textAlpha;
+        this.iconResource = iconResource;
         makePaints();
+        loadImages(iconResource);
+    }
+
+    private void loadImages(int image) {
+        if (image != 0) {
+            icon = BitmapFactory.decodeResource(getResources(), image);
+        }
+    }
+
+    private void resizeIcon(int side) {
+        if (icon != null) {
+            drawIcon = Bitmap.createScaledBitmap(icon, side, side, true);
+            iconX = centerX - side / 2;
+            iconY = centerY - side / 2;
+        }
     }
 
     public void setText(String text) {
@@ -53,9 +78,9 @@ public class ViewDeckCircle extends View {
 
     private void measureText() {
         if (text != null) {
-            markerPaint.setTextSize(radius / 3 * 2);
+            textPaint.setTextSize(radius / 3 * 2);
             Rect textBounds = new Rect();
-            markerPaint.getTextBounds("A", 0, 1, textBounds);
+            textPaint.getTextBounds("A", 0, 1, textBounds);
             float textWidth = textBounds.width();
             float textHeight = textBounds.height();
             textX = centerX - textWidth * text.length() / 2;
@@ -70,10 +95,12 @@ public class ViewDeckCircle extends View {
             radius = viewHeight / 2;
             centerX = viewWidth / 2;
             centerY = viewHeight / 2;
+            resizeIcon((int) (viewHeight * iconScale));
         } else {
             radius = viewWidth / 2;
             centerX = viewWidth / 2;
             centerY = viewHeight / 2;
+            resizeIcon((int) (viewWidth * iconScale));
         }
         measureText();
         //LogUtil.logDeubg(this, "updateDimension: " + viewWidth + " " + viewHeight);
@@ -91,23 +118,35 @@ public class ViewDeckCircle extends View {
         shadePaint.setColor(color);
         shadePaint.setAlpha(alpha);
 
-        markerPaint = new Paint();
-        markerPaint.setAntiAlias(true);
-        markerPaint.setStyle(Paint.Style.FILL);
-        markerPaint.setColor(markerColor);
-        markerPaint.setAlpha(markerAlpha);
-        markerPaint.setStrokeWidth(5);
+        if (textColor != 0) {
+            textPaint = new Paint();
+            textPaint.setAntiAlias(true);
+            textPaint.setStyle(Paint.Style.FILL);
+            textPaint.setColor(textColor);
+            textPaint.setAlpha(textAlpha);
+            textPaint.setStrokeWidth(5);
+        }
+
+        if (iconResource != 0) {
+            iconPaint = new Paint();
+            iconPaint.setAntiAlias(true);
+            iconPaint.setFilterBitmap(true);
+            iconPaint.setDither(true);
+            if (iconColor != 0) {
+                ColorFilter filter = new PorterDuffColorFilter(iconColor, PorterDuff.Mode.MULTIPLY);
+                iconPaint.setColorFilter(filter);
+            }
+        }
     }
 
     @Override
     public void onDraw(Canvas canvas) {
         canvas.drawCircle(centerX, centerY, radius, shadePaint);
-        if (drawMarker) {
-            canvas.drawLine(centerX, centerY - radius / 3, centerX, centerY + radius / 3, markerPaint);
-            canvas.drawLine(centerX - radius / 3, centerY, centerX + radius / 3, centerY, markerPaint);
-        }
         if (text != null) {
-            canvas.drawText(text, textX, textY, markerPaint);
+            canvas.drawText(text, textX, textY, textPaint);
+        }
+        if (drawIcon != null) {
+            canvas.drawBitmap(drawIcon, iconX, iconY, iconPaint);
         }
     }
 }
