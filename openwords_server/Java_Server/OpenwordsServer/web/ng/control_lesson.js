e489fe36d78a93d38d8dc0fc75ee528a9bead7cd
Shenshen Han <hanaldo520124@gmail.com>
2016-03-26 20:15:34 -0400

    Client UI Preview for Lesson.

--- openwords_server/Java_Server/OpenwordsServer/web/ng/control_lesson.js
+++ openwords_server/Java_Server/OpenwordsServer/web/ng/control_lesson.js
@@ -1,4 +1,4 @@
-App.controller("RootControl", function($scope) {
+App.controller("RootControl", function($scope, $sce) {
 
     $scope.lesson = [
         {
@@ -11,7 +11,23 @@ App.controller("RootControl", function($scope) {
                 {items: ["星期一"], tags: []}
             ],
             marplots: {items: ["明天", "昨天", "星期日"], tags: []},
-            done: true
+            done: true,
+            answerDisplay: ["星期日", "星期一", "昨天", "今天", "明天", "今日"]
+        },
+        {
+            problemLines: [
+                {items: ["I am a cat."], tags: []},
+                {items: ["@1", "@2", "@3", "@4"], tags: []}
+            ],
+            answers: [
+                {items: ["我"], tags: []},
+                {items: ["是"], tags: []},
+                {items: ["一只"], tags: []},
+                {items: ["猫。"], tags: []}
+            ],
+            marplots: {items: ["你", "不是", "狗。", "一支"], tags: []},
+            done: true,
+            answerDisplay: ["猫。", "一支", "我", "是", "一只", "狗。", "你", "不是"]
         },
         {
             problemLines: [
@@ -29,7 +45,8 @@ App.controller("RootControl", function($scope) {
             problemLines: [{items: [], tags: []}],
             answers: [],
             marplots: {items: [], tags: []},
-            done: false
+            done: false,
+            answerDisplay: []
         });
     };
 
@@ -65,6 +82,27 @@ App.controller("RootControl", function($scope) {
         });
     };
 
+    $scope.addBlankItem = function(problem, line) {
+        var blankOrder = 1;
+        line.tags.forEach(function(tag) {
+            if (tag.text.indexOf("@") > -1) {
+                blankOrder += 1;
+            }
+        });
+        line.tags.push({text: "@" + blankOrder});
+
+        problem.answers = [];
+        problem.problemLines.forEach(function(line) {
+            line.tags.forEach(function(tag) {
+                if (tag.text.indexOf("@") > -1) {
+                    problem.answers.push({
+                        items: [], tags: []
+                    });
+                }
+            });
+        });
+    };
+
     $scope.confirmProblem = function(problem) {
         problem.problemLines.forEach(function(line) {
             line.tags.forEach(function(tag) {
@@ -72,23 +110,47 @@ App.controller("RootControl", function($scope) {
             });
         });
 
+        problem.answerDisplay = [];
         problem.answers.forEach(function(answer) {
             answer.tags.forEach(function(tag) {
                 answer.items.push(tag.text);
+                problem.answerDisplay.push(tag.text);
             });
         });
 
         problem.marplots.tags.forEach(function(tag) {
             problem.marplots.items.push(tag.text);
+            problem.answerDisplay.push(tag.text);
         });
 
         problem.done = true;
+        problem.answerDisplay.sort(function() {
+            return 0.5 - Math.random();
+        });
+    };
+
+    $scope.displayProblemLine = function(line) {
+        var html = "";
+        var totalBlank = 0;
+        line.items.forEach(function(item) {
+            if (S(item).contains("@")) {
+                totalBlank += 1;
+                html += "<span class='form-inline'><input type='text' style='width: 50px; color: #000000;' class='form-control'/></span>";
+            } else {
+                html += "<span>" + item + "</span>";
+            }
+        });
+        if (totalBlank === line.items.length) {
+            console.log("big blank");
+            return $sce.trustAsHtml("<input type='text' style='width: 120px; color: #000000;' class='form-control'/>");
+        }
+        return $sce.trustAsHtml(html);
     };
 
     $scope.downloadLesson = function() {
         var out = "";
         $scope.lesson.forEach(function(problem) {
-            out += "=fb\n";
+            out += "=fb\r\n";
 
             problem.problemLines.forEach(function(line) {
                 out += "*";
@@ -99,7 +161,7 @@ App.controller("RootControl", function($scope) {
                         out += "[" + item + "]";
                     }
                 });
-                out += "\n";
+                out += "\r\n";
             });
 
             problem.answers.forEach(function(answer) {
@@ -107,7 +169,7 @@ App.controller("RootControl", function($scope) {
                 answer.items.forEach(function(item) {
                     out += "[" + item + "]";
                 });
-                out += "\n";
+                out += "\r\n";
             });
 
             out += "%";
@@ -115,7 +177,7 @@ App.controller("RootControl", function($scope) {
                 out += "[" + item + "]";
             });
 
-            out += "\n\n\n";
+            out += "\r\n\r\n\r\n";
         });
 
         var blob = new Blob([out], {type: "text/plain;charset=utf-8"});
