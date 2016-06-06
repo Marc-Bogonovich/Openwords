ef83e8e656c027a8f1ddbbffa84746ca496a6c2e
Shenshen Han <hanaldo520124@gmail.com>
2016-06-06 10:31:38 -0400

    Cool page-swipe and card-list.

--- openwords_server/Java_Server/OpenwordsServer/web/mobile/my-app.js
+++ openwords_server/Java_Server/OpenwordsServer/web/mobile/my-app.js
@@ -1,6 +1,9 @@
 // Initialize app and store it to myApp variable for futher access to its methods
 var myApp = new Framework7({
-    swipePanel: "right"
+    swipePanel: "right",
+    showBarsOnPageScrollEnd: false,
+    hideNavbarOnPageScroll: true,
+    showBarsOnPageScrollTop: false
 });
 
 // We need to use custom DOM library, let's save it to $$ variable:
@@ -14,3 +17,11 @@ var mainView = myApp.addView(".view-main", {
     dynamicNavbar: true,
     domCache: true
 });
+
+var stepsUI;
+myApp.onPageInit("steps", function(page) {
+    stepsUI = myApp.swiper(".swiper-container", {
+        pagination: ".swiper-pagination"
+    });
+});
+
