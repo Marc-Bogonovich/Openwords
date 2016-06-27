944899689fe7bd0e13a46c95fc68ce7c3b30c861
Shenshen Han <hanaldo520124@gmail.com>
2016-06-27 18:14:40 -0400

    Lesson upload service.

---
+++ openwords_server/Java_Server/OpenwordsServer/src/java/com/openwords/actions/course/UploadLesson.java
@@ -0,0 +1,77 @@
+package com.openwords.actions.course;
+
+import com.openwords.database.DatabaseHandler;
+import com.openwords.database.StepContent;
+import com.openwords.interfaces.MyAction;
+import com.openwords.utils.MyGson;
+import com.openwords.utils.UtilLog;
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import org.apache.struts2.convention.annotation.Action;
+import org.hibernate.Session;
+
+public class UploadLesson extends MyAction {
+
+    private static final long serialVersionUID = 1L;
+    private File file;
+    private String userId, name;
+
+    @Action(value = "/uploadLesson")
+    @Override
+    public String execute() throws Exception {
+        UtilLog.logInfo(this, "/uploadLesson: " + userId + " " + name);
+        if (file == null) {
+            sendBadRequest("Need actual file");
+            return null;
+        }
+        if (userId == null) {
+            sendBadRequest("Need user info");
+            return null;
+        }
+        if (name == null) {
+            sendBadRequest("Need name");
+            return null;
+        }
+
+        Session s = DatabaseHandler.getSession();
+        try {
+            long now = System.currentTimeMillis();
+
+            Scanner scan = new Scanner(file);
+            List<String> lines = new LinkedList<>();
+            while (scan.hasNextLine()) {
+                lines.add(scan.nextLine().trim());
+            }
+            scan.close();
+
+            List<StepContent> steps = LessonContentConverter.getStepsFromText(lines);
+            System.out.println(MyGson.toPrettyJson(steps));
+
+        } catch (Exception e) {
+            UtilLog.logError(this, e);
+            sendError(e.toString());
+        } finally {
+            s.close();
+        }
+        return null;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setErrorMessage(String errorMessage) {
+    }
+
+}
