b66437e30ff3b5bd35bfc0a87e2f12975f8be45b
Shenshen Han <hanaldo520124@gmail.com>
2016-08-12 13:11:55 -0400

    Better StepsControl.

--- openwords_server/Java_Server/OpenwordsServer/web/mobile/ng/control_steps.js
+++ openwords_server/Java_Server/OpenwordsServer/web/mobile/ng/control_steps.js
@@ -1,10 +1,35 @@
 myNg.controller("StepsControl", function($scope, $http) {
-    $scope.steps = [];
+    $scope.lesson;
+    $scope.mode;
+});
 
-    $scope.setSteps = function(s) {
-        console.log("setSteps");
-        $scope.steps = s;
+myNg.controller("StepPageControl", function($scope) {
+    $scope.myIndex;
+    $scope.step;
+    $scope.init = function(index) {
+        $scope.myIndex = index;
+        console.log("hi im " + index);
+        console.log("mode " + $scope.mode);
+        $scope.step = STEPS[index];
+
+        $scope.step.lines.forEach(function(line) {
+            line.forEach(function(item) {
+                if (item.type === "ans") {
+                    item.text.forEach(function(answer) {
+                        $scope.answerPool.push(answer);
+                    });
+                }
+            });
+        });
+
+        $scope.step.marplots.forEach(function(group) {
+            group.text.forEach(function(mar) {
+                $scope.answerPool.push(mar);
+            });
+        });
     };
+
+    $scope.answerPool = [];
 });
 
 
