48521ce4044d248c476e2d21f7311d3d598808e2
Shenshen Han <hanaldo520124@gmail.com>
2016-08-17 11:21:08 -0400

    Move some frequent objects to Root scope.

--- openwords_server/Java_Server/OpenwordsServer/web/mobile/ng/control_root.js
+++ openwords_server/Java_Server/OpenwordsServer/web/mobile/ng/control_root.js
@@ -1,4 +1,4 @@
-myNg.controller("RootControl", function($scope, $http, $compile) {
+myNg.controller("RootControl", function($scope, $http, $httpParamSerializerJQLike) {
     $scope.logOut = function() {
         mainView.router.load({pageName: "index"});
     };
@@ -16,6 +16,45 @@ myNg.controller("RootControl", function($scope, $http, $compile) {
     $scope.rootAllCourse = {
         page: 1,
         pageSize: 10000
+    };
+
+    $scope.rootTargetCourse = {
+        target: null
+    };
+    $scope.addLessonFromPool = function(les) {
+        if ($scope.rootTargetCourse.target) {
+            $scope.rootTargetCourse.target.json.lessons.push(JSON.parse(angular.toJson(les)));
+        }
+    };
+
+    $scope.saveCourse = function() {
+        if (!$scope.rootTargetCourse.target.name) {
+            myApp.alert(null, "Course must have a name!");
+            return;
+        }
+        console.log($scope.rootTargetCourse.target);
+        $scope.rootTargetCourse.target.content = angular.toJson($scope.rootTargetCourse.target.json);
+        $scope.rootTargetCourse.target.json = null;
+        $http({
+            url: "saveCourse",
+            method: "post",
+            headers: {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
+            data: $httpParamSerializerJQLike({
+                course: angular.toJson($scope.rootTargetCourse.target)
+            })
+        }).then(function(res) {
+            if (res.data.errorMessage) {
+                myApp.alert(null, "Change made failed");
+            } else {
+                myApp.alert(null, "Change made success");
+                $scope.rootTargetCourse.target = null;
+
+                getScope("CourseManagerControl").listMyCourses(1);
+                getScope("CourseListControl").listCourses(1);
+
+                mainView.router.load({pageName: "course_manager"});
+            }
+        });
     };
 
 });
