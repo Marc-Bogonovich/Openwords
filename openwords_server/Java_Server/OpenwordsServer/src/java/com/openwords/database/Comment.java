af0a031a9503c4a10f64042ada2ae7741dbfcd63
Shenshen Han <hanaldo520124@gmail.com>
2016-08-26 21:36:52 -0400

    Fix.

--- openwords_server/Java_Server/OpenwordsServer/src/java/com/openwords/database/Comment.java
+++ openwords_server/Java_Server/OpenwordsServer/src/java/com/openwords/database/Comment.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
-@Table(name = "Comment")
+@Table(name = "comment")
 public class Comment implements Serializable {
 
     private static final long serialVersionUID = 1L;
