6a6be5d6285b5006fa0786f06b13e55b7460590b
Shenshen Han <hanaldo520124@gmail.com>
2016-08-29 16:55:53 -0400

    Fix.

--- openwords_server/Java_Server/OpenwordsServer/src/java/com/openwords/database/Lesson.java
+++ openwords_server/Java_Server/OpenwordsServer/src/java/com/openwords/database/Lesson.java
@@ -16,14 +16,14 @@ import org.hibernate.criterion.Restrictions;
 @Entity
 @Table(name = "lessons")
 public class Lesson implements Serializable {
-    
+
     private static final long serialVersionUID = 1L;
-    
+
     public static Lesson getLesson(Session s, long userId, String name) {
         @SuppressWarnings("unchecked")
         List<Lesson> r = s.createCriteria(Lesson.class)
                 .add(Restrictions.eq("userId", userId))
-                .add(Restrictions.eq("userId", userId))
+                .add(Restrictions.eq("name", name))
                 .list();
         if (r.isEmpty()) {
             return null;
@@ -33,10 +33,10 @@ public class Lesson implements Serializable {
     private long userId, updated;
     private String name, content, langOne, langTwo;
     private Map<String, Object> json;
-    
+
     public Lesson() {
     }
-    
+
     public Lesson(long userId, String name, String langOne, String langTwo, long updated) {
         this.userId = userId;
         this.updated = updated;
@@ -44,64 +44,64 @@ public class Lesson implements Serializable {
         this.langOne = langOne;
         this.langTwo = langTwo;
     }
-    
+
     @Id
     @Column(name = "user_id")
     public long getUserId() {
         return userId;
     }
-    
+
     public void setUserId(long userId) {
         this.userId = userId;
     }
-    
+
     @Id
     @Column(name = "lesson_name")
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
-    
+
     @Column(name = "language_one")
     public String getLangOne() {
         return langOne;
     }
-    
+
     public void setLangOne(String langOne) {
         this.langOne = langOne;
     }
-    
+
     @Column(name = "language_two")
     public String getLangTwo() {
         return langTwo;
     }
-    
+
     public void setLangTwo(String langTwo) {
         this.langTwo = langTwo;
     }
-    
+
     @Column(name = "content")
     @JSON(serialize = false, deserialize = false)
     public String getContent() {
         return content;
     }
-    
+
     public void setContent(String content) {
         this.content = content;
     }
-    
+
     @Column(name = "updated_time")
     public long getUpdated() {
         return updated;
     }
-    
+
     public void setUpdated(long updated) {
         this.updated = updated;
     }
-    
+
     @Transient
     public Map<String, Object> getJson() {
         if (json == null) {
@@ -109,5 +109,5 @@ public class Lesson implements Serializable {
         }
         return json;
     }
-    
+
 }
